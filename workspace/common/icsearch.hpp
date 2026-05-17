d23ada1b4b7c17eaeec678579e76a8ab5b38ce1f
zycpopo <3368185928@qq.com>
2026-05-17 19:28:58 +0800

    ES二次封装

--- workspace/common/icsearch.hpp
+++ workspace/common/icsearch.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cpr/cpr.h>
 #include <json/json.h>
+#include <elasticlient/client.h>
 #include <memory>
 #include "logger.hpp"
 
@@ -37,8 +38,9 @@ bool UnSerialize(const std::string &src, Json::Value &val)
 
 class ESIndex{
     public:
-        ESIndex(const std::string ,const std::string &type)
-        {
+        ESIndex(std::shared_ptr<elasticlient::Client> &client,
+            const std::string & name,const std::string &type):
+            _name(name),_type(type),_client(client){
             Json::Value analysis;
             Json::Value analyzer;
             Json::Value ik;
@@ -46,21 +48,58 @@ class ESIndex{
             tokenizer["tokenizer"] = "ik_max_word";
             ik["ik"] = tokenizer;
             analyzer["analyzer"] = ik;
-            analysis["analyzer"] = analyzer;
+            analysis["analysis"] = analyzer;
             _index["settings"] = analysis;
 
+            
+        }
+        ESIndex& append(const std::string &key,
+            const std::string &type = "text",
+            const std::string &analyzer = "ik_max_word",
+            bool enabled = true){
+                Json::Value fields;
+                
+                fields["type"] = type;
+                fields["analyzer"] = analyzer;
+                if (enabled == false) fields["enabled"] = enabled;
+                _properties[key] = fields;
+                return *this;
+        }
+
+        bool create()
+        {
             Json::Value mappings;
             mappings["dynamic"] = true;
+            mappings["properties"] = _properties;
+            _index["mappings"] = mappings;
 
-            std::string str;
-            Serialize(_index,str);
-            std::cout << str << std::endl;
-        }
-        void append(const std::string &key,
-            const std::string &type = "test",
-            const std::string &ayalyzer = "ik_max_word",
-            bool enabled = true){
+            std::string body;
+            bool ret = Serialize(_index,body);
+            if(ret == false){
+                LOG_ERROR("索引序列化失败");
+                return false;
+            }
+            LOG_INFO("请求正文:[{}]",body);
 
+            //2.发起搜索请求
+            try{
+                std::cout << (uint64_t)_client.get() <<std::endl;
+                auto rsp = _client->index(_name,_type,"",body);
+                //3.打印响应状态码和响应正文
+                if(rsp.status_code < 200 || rsp.status_code>=300)
+                {
+                    LOG_ERROR("创建ES索引{}失败,响应状态码异常{}",_name,rsp.status_code);
+                    return false;
+                }
+                std::cout << rsp.status_code << std::endl;
+                std::cout << rsp.text << std::endl;
+            }catch(std::exception &e){
+                std::cout << "请求失败" << e.what() <<std::endl;
+                LOG_ERROR("创建ES索引{}失败：{}",_name,e.what());
+                return false;
+            }
+            LOG_DEBUG("测试索引创建成功！")
+            return true;
         }
 
     private:
@@ -68,4 +107,5 @@ class ESIndex{
         std::string _type;
         Json::Value _properties;
         Json::Value _index;
+        std::shared_ptr<elasticlient::Client> _client;
 };
