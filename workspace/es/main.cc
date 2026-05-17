9610141e9145c3ec3c7604b5d89017ba137affd1
zycpopo <3368185928@qq.com>
2026-05-17 19:26:27 +0800

    ES二次封装

--- workspace/es/main.cc
+++ workspace/es/main.cc
@@ -1,5 +1,6 @@
 #include "../common/icsearch.hpp"
 #include <gflags/gflags.h>
+#include <iostream>
 
 DEFINE_bool (run_mode,false,"程序运行模式,false为调试模式,true为发布模式");
 DEFINE_string(log_file,"","发布模式下，用于指定日志的输出文件");
@@ -10,6 +11,15 @@ int main(int argc,char *argv[])
     google::ParseCommandLineFlags(&argc, &argv, true);
     init_logger(FLAGS_run_mode,FLAGS_log_file, FLAGS_log_level);
 
-    ESIndex index("user","_doc");
+    std::vector<std::string> host_lists = {"http://127.0.0.1:9200/"};
+
+    auto client  = std::make_shared<elasticlient::Client>(host_lists);
+    std::cout << (uint64_t)client.get() <<std::endl;
+    bool ret = ESIndex(client,"test_user","_doc").append("nickname")
+        .append("phonenumber","keyword","standard",true)
+        .create();
+    if(ret == true){
+        LOG_DEBUG("索引创建成功");
+    }
     return 0;
 }
